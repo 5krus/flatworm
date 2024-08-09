19fa5a9214780889ff6b7743011fddb8b2685b42
Eryk Krusinski <ek620@cam.ac.uk>
2024-08-09 09:05:36 +0100

    Fixing dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = 'Tools designed for non-techy people who must write code.'
 LONG_DESCRIPTION = 'A set of packages with eye-wateringly minimalistic user interfaces, but extremely powerful and performant backends.'
 
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['os', 'joblib', 'pandas', 'numpy', 'scikit-learn', ''],
+    install_requires=['joblib', 'pandas', 'numpy', 'scikit-learn'],
     keywords=['python', 'machine learning', 'version control', 'minimalism'],
     classifiers=[
     ]
